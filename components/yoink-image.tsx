b7676d10feb34f2d069138f7787bfff9d49bc4f6
0xSumaa <0xSumaa@gmail.com>
2024-09-24 03:15:36 -0700

    NodeNext module resolution

--- components/yoink-image.tsx
+++ components/yoink-image.tsx
@@ -1,5 +1,5 @@
 import { BigNumber } from "ethers";
-import { formatBalance } from "../utils/format";
+import { formatBalance } from "../utils/format.js";
 
 type ApprovedMessageProps = {
   userBalance: BigNumber;
